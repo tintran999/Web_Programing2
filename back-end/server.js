905b0ead7c7ca862f6791d7ea858475b2e17c55a
tintran999 <tintdgcs220477@fpt.edu.vn>
2024-11-07 13:31:49 +0700

    first commit

---
+++ back-end/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+global.Vocab = require('./api/models/vocabModel');
+const routes = require('./api/routes/vocabRoutes');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb+srv://tintdgcs220477:ductin123@fgwweb2.jpmfq.mongodb.net/?retryWrites=true&w=majority&appName=FGWWeb2', { useNewUrlParser: true });
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+routes(app);
+
+app.listen(port);
+
+app.use((req, res) => {
+    res.status(404).send({ url: `${req.originalUrl} not found` });
+});
+
+console.log(`Server started on port ${port}`);
